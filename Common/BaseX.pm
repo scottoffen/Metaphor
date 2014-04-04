8843b51cb256e2dd60d30cc6384bae63c23a5f85
Scott Offen <github@scottoffen.com>
2014-04-04 14:27:09 -0600

    POD Updates

--- Common/BaseX.pm
+++ Common/BaseX.pm
@@ -46,7 +46,7 @@ sub new
 	#----------------------------------------------------------------------------------#
 	if (@_)
 	{
-		my @digits = @_;
+		my @digits = GetCharacterSet(@_);
 		my $digits = scalar @digits;
 
 		if ($digits > 1)
@@ -95,7 +95,7 @@ sub ConvertToBase10
 	my $base_x = shift;
 	my $base10 = 0;
 
-	my @digits = split(//, $base_x);
+	my @digits = GetDigits($base_x, $self->{baseX});
 	my $power  = scalar @digits;
 
 	for (my $i = 0; $i < scalar @digits; $i++)
@@ -119,14 +119,17 @@ sub ConvertFromBase10
 	my $base10 = shift;
 	my $base_x = undef;
 
-	while ($base10 >= $self->{base})
+	if ($base10 =~ /^\d+$/)
 	{
-		my $digit = $base10 % $self->{base};
-		$base_x = (defined $base_x) ? $self->{base10}->{$digit} . $base_x : $self->{base10}->{$digit};
-		$base10 = int($base10 / $self->{base});
-	}
+		while ($base10 >= $self->{base})
+		{
+			my $digit = $base10 % $self->{base};
+			$base_x = (defined $base_x) ? $self->{base10}->{$digit} . $base_x : $self->{base10}->{$digit};
+			$base10 = int($base10 / $self->{base});
+		}
 
-	$base_x = $self->{base10}->{$base10} . $base_x;
+		$base_x = $self->{base10}->{$base10} . $base_x;
+	}
 
 	return $base_x;
 }
@@ -134,4 +137,149 @@ sub ConvertFromBase10
 
 
 
+############################|     GetCharacterSet     |#############################
+# Private                                                                          #
+#----------------------------------------------------------------------------------#
+sub GetCharacterSet
+{
+	my $input = join('', @_);
+
+	if (length $input > 0)
+	{
+		my (%unique, @unique);
+		my @charset = split('', $input);
+
+		foreach my $char (@charset)
+		{
+			my $ascii = ord($char);
+
+			if (($ascii > 31) && ($ascii < 127))
+			{
+				if (!exists $unique{$char})
+				{
+					$unique{$char} = 1;
+					push(@unique, $char);
+				}
+			}
+
+		}
+
+		return @unique;
+	}
+
+	return undef;
+}
+#########################################||#########################################
+
+
+
+###############################|     GetDigits     |################################
+# Private                                                                          #
+#----------------------------------------------------------------------------------#
+sub GetDigits
+{
+	my ($input, $valid) = @_;
+
+	$valid = join('', keys %$valid);
+	$input =~ s/[^$valid]//g;
+
+	return split('', $input);
+}
+#########################################||#########################################
+
+
+
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Common::BaseX - Allows for integer conversion of arbitrary numbering systems between base I<x> and base 10.
+
+=head1 SYNOPSIS
+
+    use Common::BaseX;
+
+    my $base17  = new Common::BaseX('0123456789abcdefg');
+    my $baseXYZ = new Common::BaseX('x', 'y', 'z');
+    my $value   = 101;
+
+    $value = $base17->ConvertFromBase10($value);
+    print "$value\n";
+
+    # output:
+    # 5g
+
+    $value = $base17->ConvertToBase10($value);
+    print "$value\n";
+
+    # output:
+    # 101
+
+    $value = $baseXYZ->ConvertFromBase10($value);
+    print "$value\n";
+
+    # output:
+    # yxzxz
+
+=head1 DESCRIPTION
+
+Create your own numbering systems using any arbitrary set of characters in any order, and convert to and from base 10 numbers!  Convert between two number systems using base 10 as an intermediary!
+
+B<Note:> This module does not convert plain text to a base x number.  If you want to convert plain text, you should first convert the text to a number.  I would suggest using L<perl's native ord function|http://perldoc.perl.org/functions/ord.html> to assist with that.
+
+=head2 Methods
+
+Only public methods are documented.
+
+=over 12
+
+=item C<new(STRING|ARRAY)>
+
+The input array is parsed into an array of L<ascii-printable characters|http://en.wikipedia.org/wiki/ASCII#ASCII_printable_characters> ordered by their first occurrence in the array and used as a numbering system.
+
+    # All three of these are using the same numbering system
+    my $foo = new Common::BaseX('scott offen');
+    my $bar = new Common::BaseX('scott', 'scott', 'offen', 'soften');
+    my $baz = new Common::BaseX('s', 'c', 'o', 't', 'f', 'e', 'n');
+
+While this means that you I<can> pass a value multiple times, it is not recommended, as this will likely end up with results you did not intend.
+
+=item C<ConvertToBase10(STRING)>
+
+Attempts to convert a string of characters to base 10 using the internal numbering system created when the object was initialized.  Any characters not in the number system will be stripped out prior to conversion.
+
+    my $abc = new Common::BaseX('abc');
+    my $val = $abc->ConvertToBase10('abc');
+
+    my $foo = $abc->ConvertToBase10('a9bc'); # Contains a value not in the numbering system
+    my $bar = $abc->ConvertToBase10('1abc'); # Contains a value not in the numbering system
+    my $baz = $abc->ConvertToBase10('abcZ'); # Contains a value not in the numbering system
+
+    print "$val = $foo = $bar = $baz";
+
+    # output
+    # 5 = 5 = 5 = 5
+
+=item C<ConvertFromBase10(INTEGER)>
+
+Attempts to convert a base 10 number to a string of characters using the internal numbering system created when the object was initialized.  Returns a string result.
+
+=back
+
+=head1 TODO
+
+At some point I'd like to implement methods that would convert plain text to/from base x number systems, but I've yet to have a need for it.
+
+=head1 AUTHOR
+
+(c) Copyright 2011-2014 Scott Offen (L<http://www.scottoffen.com/>)
+
+=head1 DEPENDENCIES
+
+L<Common::Logging|https://github.com/scottoffen/Common-Perl/wiki/Common::Logging>
+
+=cut
--- Common/Tripcode.pm
+++ Common/Tripcode.pm
@@ -118,3 +118,91 @@ sub GetTripPhrase
 
 
 1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Common::Tripcode - Implements a basic tripcode algorithm for pseudo-registration systems.
+
+=head1 SYNOPSIS
+
+    use Common::Tripcode; # exports GetTripcode by default
+
+    my $tripcode = GetTripcode('name#password');
+    print $tripcode;
+
+    # output:
+    # name!zwK2bfx.2U
+
+=head1 DESCRIPTION
+
+Tripcodes can help verify a user's identity to others, and are a type of pseudo-registration. To use a tripcode, append a name (e.g., username or real name) with a hash mark ("#") followed by a word or short phrase (e.g. "name#password"). A hash unique to that particular word or phrase will be generated. The previous example would return "name!zwK2bfx.2U".
+
+B<NOTE: Tripcodes are not secure, and can be cracked with relative ease.  This module does not support "secure" tripcodes.>
+
+=head2 Algorithm
+
+The algorithm used to generate the tripcode is outlined below, and is a variation on the one described on L<wikipedia|http://en.wikipedia.org/wiki/File:Tripcode_generation_diagram.svg> here.
+
+1. Split username and password on the first occurrence of the '#'.
+
+2. Generate salt by taking the second and third characters of the string produced after appending the name part with 'H..'. [1]
+
+3. Convert password part to ShiftJIS.
+
+4. Replace any character in password part not between ASCII 46 (.) and 122 (z) inclusive with a period (.).
+
+5. Replace any of the characters in the set :;<=>?@[]^_` found in the password part with the corresponding character from the set ABCDEFGabcdef.
+
+6. Call the crypt() function with the input and salt.
+
+7. Return the last 10 characters of the output of crypt (compressional data harvest).
+
+[1] By deriving salt from the name instead of the password, we decrease the statistical likelihood that two people who use the same password will have generated the same tripcode, and that two identical tripcodes will have been resultant of the same password.
+
+=head2 Methods
+
+Only public methods are documented.
+
+=over 12
+
+=item C<GetTripcode(NameAndPassword [, TripcodeSeperator])>
+
+I<[Exported]>
+
+The I<NameAndPassword> input string is expected to be in the format of 'name#password'.  Returns a string with the tripcode in the format of 'name!tripcode'.  Returns I<undef> if the input string cannot be parsed.
+
+The default tripcode separator is the bang (!).  An alternate tripcode separator can be passed in as a second argument, in which case it will be used instead.
+
+    use Common::Tripcode;
+
+    my $tripcode1 = GetTripcode('name#password');
+    my $tripcode2 = GetTripcode('name#password', '%');
+    print "$tripcode1\n$tripcode2";
+
+    # output:
+    # name!zwK2bfx.2U
+    # name%zwK2bfx.2U
+
+Use of an alternate tripcode separator does not impact the generation of the tripcode.
+
+=back
+
+=head1 TODO
+
+Implement I<GetTripPhrase> method described L<here|http://worrydream.com/tripphrase/>.
+
+=head1 AUTHOR
+
+(c) Copyright 2011-2014 Scott Offen (L<http://www.scottoffen.com/>)
+
+=head1 SEE ALSO
+
+Using L<Encode> to convert the password to L<Shift JIS|http://en.wikipedia.org/wiki/Shift_JIS>.
+
+My algorithm is loosely based on this L<tripcode generation diagram|http://en.wikipedia.org/wiki/File:Tripcode_generation_diagram.svg> on wikipedia.
+
+=cut
